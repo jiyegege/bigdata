afc163cb018838afd8536dd55068723c3e1f879a
roger <1468667058@qq.com>
2020-08-18 22:58:50 +0800

    feat：fist commit

---
+++ src/test/java/top/roger/big_data/BigDataApplicationTests.java
@@ -0,0 +1,13 @@
+package top.roger.big_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
